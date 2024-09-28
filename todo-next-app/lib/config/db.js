d0d97f93db2417225f86b7b165b17ac1b5439357
Raiyan Hossain <163441987+raiiyan@users.noreply.github.com>
2024-09-28 18:22:58 +0600

    Delete todo-next-app directory

--- todo-next-app/lib/config/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const ConnectDB = async () => {
-    await mongoose.connect('mongodb+srv://raiyanhossain:aG4uJuPwMkDdHk9o@cluster0.blacp.mongodb.net/todo-app');
-    console.log("DB Connected");
-}
