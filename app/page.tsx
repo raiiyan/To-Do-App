acb8b6e3b56822c3a37b82882e03b908dd394aaa
Raiyan Hossain <raiyanhossain93@gmail.com>
2024-10-01 00:24:57 +0600

    4th

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useEffect, useState } from 'react';
-import Todo from '../Components/Todo'
+import Todo from '../Components/Todo';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import axios from 'axios';
